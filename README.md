90ead70eddda075c51204fa3a9ea3ce4c887153d
SOMRAT <monnaf37@gmail.com>
2018-09-12 15:40:04 +0600

    initial release

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Meghna_ for Hugo
+![screenshot](https://user-images.githubusercontent.com/37659754/45415821-501e8480-b6a0-11e8-89a8-5c2248b35d5e.png "Home of the website")
+
+Meghna is a free responsive one page business template built with HTML5/CSS3, JavaScript and JQuery. The HTML template is minimalist, lightweight and fast loading, has been shipped wrapping latest technologies and awesome design trend. Mehna has successfully passed W3C validation and speed test. The bootstrap based HTML crafted with eye-catching design(UI) and ideal for well-researched user experience (UX), perfectly suited for Business, agency, startup, corporate, portfolio, showcasing, photography studio, SME business, non-profit organizations, fashion government agencies. and many more.
+
+## Usage
+
+Create a new Hugo site by typing
+
+```
+  $ hugo new site <SITENAME>
+```
+
+Then clone this repository:
+
+```
+  $ cd <SITENAME>/themes
+  $ git clone https://github.com/themefisher/meghna-hugo
+```
+
+Now take a look at the exampleSite folder and you're ready to go!
+
+## License
+
+This Template is released under [Creative Commons Attribution 3.0 (CC-BY-3.0) License](https://creativecommons.org/licenses/by/3.0/)
+If you want to remove the credit simply make a [donation](https://www.paypal.me/Themefisher), so that we can run our contribution to hugo community.
+
+[![HELP US TO GROW](https://user-images.githubusercontent.com/16266381/45262626-1e0ce880-b43c-11e8-9698-1b95f143e240.png)](https://www.paypal.me/Themefisher)
+
+## About Themefisher
+
+[Themefisher] has been creating beautiful free and premium Bootstrap and HTML5 templates since 2014.
+Visit us at https://themefisher.com/ to see other great-looking themes.
+
+[Hugo]: https://gohugo.io/
+[Themefisher]: https://themefisher.com/
+
+## We are available for Hiring of your next HUGO project. Drop Us a mail [themefisher@gmail.com](mailto:themefisher@gmail.com)
---
+++ static/js/script.js
@@ -0,0 +1,309 @@
+jQuery(function ($) { "use strict";
+	
+	/* ========================================================================= */
+	/*	Page Preloader
+	/* ========================================================================= */
+	
+	window.onload = function () {
+		document.getElementById('preloader').style.display = 'none';
+	}
+
+
+	/* ========================================================================= */
+	/*	Post image slider
+	/* ========================================================================= */
+	
+	$("#post-thumb, #gallery-post").slick({
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 4000
+		
+	});
+	
+	$("#features").slick({
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 4000
+	});
+
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+
+	$("#navigation").sticky({
+		topSpacing : 0
+	});
+	
+
+	/* ========================================================================= */
+	/*	Magnific popup
+	/* =========================================================================  */
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    removalDelay: 160, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            // just a hack that adds mfp-anim class to markup
+            this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    closeOnContentClick: true,
+    midClick: true,
+    fixedContentPos: false,
+    fixedBgPos: true
+});
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+
+  	var mixer = mixitup('.portfolio-items-wrapper');
+	
+	/* ========================================================================= */
+	/*	Testimonial Carousel
+	/* =========================================================================  */
+ 
+	//Init the carousel
+	$("#testimonials").slick({
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 4000
+	});
+
+
+
+
+
+	/* ========================================================================= */
+	/*   Contact Form Validating
+	/* ========================================================================= */
+
+
+	$('#contact-submit').click(function (e) {
+
+		//stop the form from being submitted
+		e.preventDefault();
+
+		/* declare the variables, var error is the variable that we use on the end
+		to determine if there was an error or not */
+		var error = false;
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var subject = $('#subject').val();
+		var message = $('#message').val();
+
+		/* in the next section we do the checking by using VARIABLE.length
+		where VARIABLE is the variable we are checking (like name, email),
+		length is a JavaScript function to get the number of characters.
+		And as you can see if the num of characters is 0 we set the error
+		variable to true and show the name_error div with the fadeIn effect. 
+		if it's not 0 then we fadeOut the div( that's if the div is shown and
+		the error is fixed it fadesOut. 
+		
+		The only difference from these checks is the email checking, we have
+		email.indexOf('@') which checks if there is @ in the email input field.
+		This JavaScript function will return -1 if no occurrence have been found.*/
+		if (name.length == 0) {
+			var error = true;
+			$('#name').css("border-color", "#D8000C");
+		} else {
+			$('#name').css("border-color", "#666");
+		}
+		if (email.length == 0 || email.indexOf('@') == '-1') {
+			var error = true;
+			$('#email').css("border-color", "#D8000C");
+		} else {
+			$('#email').css("border-color", "#666");
+		}
+		if (subject.length == 0) {
+			var error = true;
+			$('#subject').css("border-color", "#D8000C");
+		} else {
+			$('#subject').css("border-color", "#666");
+		}
+		if (message.length == 0) {
+			var error = true;
+			$('#message').css("border-color", "#D8000C");
+		} else {
+			$('#message').css("border-color", "#666");
+		}
+
+		//now when the validation is done we check if the error variable is false (no errors)
+		if (error == false) {
+			//disable the submit button to avoid spamming
+			//and change the button text to Sending...
+			$('#contact-submit').attr({
+				'disabled': 'false',
+				'value': 'Sending...'
+			});
+
+			/* using the jquery's post(ajax) function and a lifesaver
+			function serialize() which gets all the data from the form
+			we submit it to send_email.php */
+			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
+				//and after the ajax request ends we check the text returned
+				if (result == 'sent') {
+					//if the mail is sent remove the submit paragraph
+					$('#cf-submit').remove();
+					//and show the mail success div with fadeIn
+					$('#mail-success').fadeIn(500);
+				} else {
+					//show the mail failed div
+					$('#mail-fail').fadeIn(500);
+					//re enable the submit button by removing attribute disabled and change the text back to Send The Message
+					$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
+				}
+			});
+		}
+	});
+
+});
+// End Jquery Function
+
+
+	/* ========================================================================= */
+	/*	Animated section
+	/* ========================================================================= */
+
+	var wow = new WOW(
+		{
+		  offset:       100,          // distance to the element when triggering the animation (default is 0)
+		  mobile:       false      // trigger animations on mobile devices (default is true)
+		}
+	 );
+	 wow.init();
+
+
+	/* ========================================================================= */
+	/*	Smooth Scroll
+	/* ========================================================================= */
+	var scroll = new SmoothScroll('a[href*="#"]');
+
+
+
+	/* ========================================================================= */
+	/*	Google Map Customization
+	/* =========================================================================  */
+
+	function initialize() {
+
+		var latitude = $('#map-canvas').attr('data-latitude');
+		var longitude = $('#map-canvas').attr('data-longitude');
+		var myLatLng = new google.maps.LatLng(latitude, longitude);
+
+		var roadAtlasStyles = [{
+			"featureType": "landscape",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#2F3238"
+			}]
+		}, {
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#FFFFFF"
+			}]
+		}, {
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#50525f"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"visibility": "on"
+			}, {
+				"color": "#808080"
+			}]
+		}, {
+			"featureType": "poi",
+			"elementType": "labels",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "transit",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#808080"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#3071a7"
+			}, {
+				"saturation": -65
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "landscape",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#bbbbbb"
+			}]
+		}];
+
+		var mapOptions = {
+			zoom: 14,
+			center: myLatLng,
+			disableDefaultUI: true,
+			scrollwheel: false,
+			navigationControl: false,
+			mapTypeControl: false,
+			scaleControl: false,
+			draggable: false,
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+			}
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: '',
+		});
+
+
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.open(map, marker);
+		});
+
+		var styledMapOptions = {
+			name: 'US Road Atlas'
+		};
+
+		var usRoadMapType = new google.maps.StyledMapType(
+			roadAtlasStyles, styledMapOptions);
+
+		map.mapTypes.set('roadatlas', usRoadMapType);
+		map.setMapTypeId('roadatlas');
+	}
+
+	google.maps.event.addDomListener(window, "load", initialize);
+
