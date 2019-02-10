5a6221f50cf83b30e080a62ea9a1417b4727da1e
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-10 13:26:03 +0100

    Googlebot crawler doesn't see the content revealed by wow.js
    https://github.com/matthieua/WOW/issues/196

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,7 +164,15 @@ var wow = new WOW({
 	offset: 100, // distance to the element when triggering the animation (default is 0)
 	mobile: false // trigger animations on mobile devices (default is true)
 });
-wow.init();
+
+//https://github.com/matthieua/WOW/issues/196#issuecomment-348734401
+var scrolled = false;
+$(window).on('scroll', function() {
+	if (!scrolled) {
+		scrolled = true;
+		wow.init();
+	}
+})
 
 /* ========================================================================= */
 /*	Google Map Customization
