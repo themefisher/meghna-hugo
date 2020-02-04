f3f332f1aea5d32c1951e03598524d1a378fb99e
Themefisher Dev <themefisher.dev@gmail.com>
2020-02-04 17:43:17 +0600

    modified navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,7 @@ jQuery(function ($) {
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				html_body.animate({
-					scrollTop: target.offset().top - 70
+					scrollTop: target.offset().top - 50
 				}, 1500, 'easeInOutExpo');
 				return false;
 			}
