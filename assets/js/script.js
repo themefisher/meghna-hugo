6de4f45aca573f1bc983fba6a656cee70c7a389b
somratpro <abdulmonnafsomrat@gmail.com>
2021-02-23 16:05:57 +0600

    modified navigation scrolling for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,6 @@ $(window).on('load', function () {
 jQuery(function ($) {
 	"use strict";
 
-
-	/* ========================================================================= */
-	/*	Auto close Navbar when click on link
-	/* ========================================================================= */
-
-	$('.navbar-collapse a').click(function () {
-		$(".navbar-collapse").collapse('hide');
-	});
-
-
 	/* ========================================================================= */
 	/*	lazy load initialize
 	/* ========================================================================= */
@@ -79,6 +69,16 @@ jQuery(function ($) {
 	/* ========================================================================= */
 	/*	animation scroll js
 	/* ========================================================================= */
+
+
+
+	function myFunction(x) {
+		if (x.matches) {
+			var topOf = 50
+		} else {
+			var topOf = 350
+		}
+	}
 
 	var html_body = $('html, body');
 	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
